ff652d9ac9210a6e2eefac5370896eb35623dc49
Nathan Ross Powell <nathanrosspowell@gmail.com>
2016-02-15 23:50:16 -0500

    Working user names
    You can change your user name.
    UI still to be tweaked.

--- main.js
+++ main.js
@@ -1,25 +1,50 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 5000;
+var app = require('express')()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var port = process.env.PORT || 5000
+
+var count = 0;
+var users = {}
 
 app.get('/', function(req, res){
-  console.log('sending out index.html');
-  res.sendFile(__dirname + '/index.html');
-});
+  console.log('sending out index.html')
+  res.sendFile(__dirname + '/index.html')
+})
 
 io.on('connection', function(socket){
-  console.log('a user connected');
+  var id = count++;
+  var name = "User " + id
+  users[socket.id] = { name:name, id:id }
+  console.log('a user connected: ' + name)
+  var values = Object.keys(users).map(function(key){
+    return users[key];
+  });
+  socket.broadcast.emit('chat_message', 'User "' + name + '" joined')
+  socket.broadcast.emit('user_add', users[socket.id])
+  socket.emit('chat_message', 'Welcome, ' + name + '!')
+  socket.emit('user_list', values)
+
+
   socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-  socket.on('chat message', function(msg){
-    console.log('emit message: ' + msg);
-    io.emit('chat message', msg);
-  });
-  io.emit('chat message', 'Welcome!');
-});
+    console.log('user disconnected')
+    io.emit('chat_message', 'User "'+ users[socket.id] + '" left.')
+    delete users[socket.id]
+  })
+
+  socket.on('chat_message', function(msg){
+    console.log('emit message: ' + msg)
+    io.emit('chat_message', users[socket.id].name + ': ' + msg)
+    socket.broadcast.emit('user_remove', users[socket.id])
+  })
+
+  socket.on('name_change', function(msg){
+    var oldName = users[socket.id].name
+    users[socket.id].name = msg
+    console.log('emit message: ' + msg)
+    io.emit('chat_message', '"' + oldName + '" changed name to  "' + msg + '"')
+  })
+})
 
 http.listen(port, function(){
-  console.log('listening on ' + port);
-});;
+  console.log('listening on ' + port)
+})
